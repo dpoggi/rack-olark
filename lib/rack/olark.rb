d22b20d4c361a541f16bc752204e3ce2a1913db4
Dan Poggi <dan.poggi@gmail.com>
2011-12-16 12:31:54 -0500

    Cleanliness is next to godliness?

--- lib/rack/olark.rb
+++ lib/rack/olark.rb
@@ -7,17 +7,25 @@ module Rack
     Defaults = {:format => :html5, :paths => []}
 
     def initialize(app, options = {})
-      raise ArgumentError, "Need a valid Olark ID!" unless options[:id] and options[:id].length.eql? 16
+      if not (options[:id] and options[:id].length.eql? 16)
+        raise ArgumentError, 'Need a valid Olark ID!'
+      end
       @app, @options = app, Defaults.merge(options)
-      @id, @format, @paths = [@options.delete(:id), @options.delete(:format), @options.delete(:paths)]
+      @id, @format, @paths = [@options.delete(:id),
+                              @options.delete(:format),
+                              @options.delete(:paths)]
 
-      @paths = @paths.class.eql?(Array) ? @paths.map do |path|
-        path.class.eql?(Regexp) ? path : /^#{Regexp.escape(path.to_s)}$/
-      end : []
+      if @paths.class.eql? Array
+        @paths = @paths.map do |path|
+          path.class.eql?(Regexp) ? path : /^#{Regexp.escape(path.to_s)}$/
+        end
+      else
+        @paths = []
+      end
 
       @option_js = "olark.identify('#{@id}');"
       @options.each do |key, val|
-        val = [String, Symbol].include? val.class ? "'#{val.to_s}'" : val.to_s
+        val = [String, Symbol].include?(val.class) ? "'#{val.to_s}'" : val.to_s
         @option_js << "olark.configure('#{key.to_s}', #{val});"
       end
     end
@@ -27,14 +35,14 @@ module Rack
     end
 
     def _call(env)
-      @status, @headers, @response = @app.call env
-      @request = Rack::Request.new env
+      @status, @headers, @response = @app.call(env)
+      @request = Rack::Request.new(env)
 
       valid_path = @paths.select {|path| @request.path_info =~ path}.length > 0
       return [@status, @headers, @response] unless html? and (@paths.empty? or valid_path)
 
-      response = Rack::Response.new [], @status, @headers
-      @response.each {|fragment| response.write inject(fragment)}
+      response = Rack::Response.new([], @status, @headers)
+      @response.each {|fragment| response.write(inject(fragment))}
       response.finish
     end
 
@@ -45,8 +53,10 @@ module Rack
     end
 
     def inject(response)
-      template_contents = ::File.read ::File.expand_path('../templates/olark.haml', __FILE__)
-      @template = Haml::Engine.new(template_contents, {:format => @format}).render self
+      template_contents = ::File.read(::File.expand_path('../templates/olark.haml',
+                                                         __FILE__))
+      @template = Haml::Engine.new(template_contents,
+                                   {:format => @format}).render(self)
       response.gsub '</body>', @template
     end
   end
